27ad77ffb4d1a5c441adae5952b5f5581ef8f887
Anurag Pal <apal895@gmail.com>
2022-01-27 12:48:41 +0530

    not answered, not edited done

--- src/utils/reducers/TestReducer.ts
+++ src/utils/reducers/TestReducer.ts
@@ -1,5 +1,6 @@
 import { TEST_ACTION, TEST_ACTION_TYPES } from "../actions";
 import { ITestsContext } from "../contexts/TestsContext";
+import { IQuestionWithID } from "../interfaces";
 import { flattenQuestions, shuffleQuestions } from "../utils";
 
 export default function TestReducer(
@@ -11,27 +12,6 @@ export default function TestReducer(
   const { questions, currentQuestion, test } = state;
 
   switch (type) {
-    case TEST_ACTION_TYPES.NEXT_QUESTION: {
-      return {
-        ...state,
-        currentQuestion:
-          currentQuestion < questions.length - 1
-            ? currentQuestion + 1
-            : currentQuestion,
-      };
-    }
-    case TEST_ACTION_TYPES.PREVIOUS_QUESTION: {
-      return {
-        ...state,
-        currentQuestion: currentQuestion > 0 ? payload - 1 : currentQuestion,
-      };
-    }
-    case TEST_ACTION_TYPES.GO_TO_QUESTION: {
-      return {
-        ...state,
-        currentQuestion: payload,
-      };
-    }
     case TEST_ACTION_TYPES.FLATTEN_QUESTIONS: {
       return {
         ...state,
@@ -49,10 +29,88 @@ export default function TestReducer(
       const allQuestionsShuffled = shuffleQuestions(allQuestionsExtracted);
       return {
         ...state,
-        questions: allQuestionsShuffled,
+        questions: allQuestionsShuffled.map((question, idx) => {
+          if (idx === 0) {
+            return {
+              ...question,
+              status: {
+                ...question.status,
+                status: "notAnswered",
+              },
+            };
+          }
+          return question;
+        }),
         status: {
           ...state.status,
           notVisitied: allQuestionsShuffled.length,
+        },
+      };
+    }
+    case TEST_ACTION_TYPES.NEXT_QUESTION: {
+      const nextIdx =
+        currentQuestion < questions.length - 1 ? payload + 1 : payload;
+      let questionVisisted = isQuestionVisited(questions[nextIdx]);
+      return {
+        ...state,
+        currentQuestion: nextIdx,
+        questions: !questionVisisted
+          ? markQuestionVisited(questions, nextIdx)
+          : questions,
+        status: {
+          ...state.status,
+          notVisitied:
+            !questionVisisted && nextIdx !== currentQuestion
+              ? state.status.notVisitied - 1
+              : state.status.notVisitied,
+          notAnswered:
+            !questionVisisted && nextIdx !== currentQuestion
+              ? state.status.notAnswered + 1
+              : state.status.notAnswered,
+        },
+      };
+    }
+    case TEST_ACTION_TYPES.PREVIOUS_QUESTION: {
+      const nextIdx = currentQuestion > 0 ? payload - 1 : currentQuestion;
+      let questionVisited = isQuestionVisited(questions[nextIdx]);
+      return {
+        ...state,
+        currentQuestion: nextIdx,
+        questions: !questionVisited
+          ? markQuestionVisited(questions, nextIdx)
+          : questions,
+        status: {
+          ...state.status,
+          notVisitied:
+            !questionVisited && nextIdx !== currentQuestion
+              ? state.status.notVisitied - 1
+              : state.status.notVisitied,
+          notAnswered:
+            !questionVisited && nextIdx !== currentQuestion
+              ? state.status.notAnswered + 1
+              : state.status.notAnswered,
+        },
+      };
+    }
+    case TEST_ACTION_TYPES.GO_TO_QUESTION: {
+      let questionVisited = isQuestionVisited(questions[payload]);
+      return {
+        ...state,
+        currentQuestion: payload,
+        questions:
+          !questionVisited && payload !== currentQuestion
+            ? markQuestionVisited(questions, payload)
+            : questions,
+        status: {
+          ...state.status,
+          notVisitied:
+            !questionVisited && payload !== currentQuestion
+              ? state.status.notVisitied - 1
+              : state.status.notVisitied,
+          notAnswered:
+            !questionVisited && payload !== currentQuestion
+              ? state.status.notAnswered + 1
+              : state.status.notAnswered,
         },
       };
     }
@@ -60,4 +118,34 @@ export default function TestReducer(
       return state;
     }
   }
+}
+
+function isQuestionVisited(question: IQuestionWithID): boolean {
+  switch (question.status.status) {
+    case "notAnswered":
+    case "answered":
+    case "markedForReview":
+    case "answeredAndMarkedForReview":
+      return true;
+    default:
+      return false;
+  }
+}
+
+function markQuestionVisited(
+  questions: Array<IQuestionWithID>,
+  qIdx: number
+): Array<IQuestionWithID> {
+  return questions.map((question, index) => {
+    if (index === qIdx) {
+      return {
+        ...question,
+        status: {
+          ...question.status,
+          status: "notAnswered",
+        },
+      };
+    }
+    return question;
+  });
 }

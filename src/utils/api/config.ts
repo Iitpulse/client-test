3ad7cd093fd9a8b75f8295bdec056b11bbefdfc1
Anurag Pal <apal895@gmail.com>
2024-02-17 10:44:10 +0530

    common user gateway

--- src/utils/api/config.ts
+++ src/utils/api/config.ts
@@ -1,26 +1,16 @@
 import axios from "axios";
 import { AUTH_TOKEN } from "../constants";
 
-export const API_QUESTIONS = () =>
+const API_GATEWAY = (service: "users" | "questions" | "tests") =>
   axios.create({
-    baseURL: `${process.env.REACT_APP_QUESTIONS_API}`,
+    baseURL: `${process.env.REACT_APP_API_GATEWAY}${service}`,
     headers: {
       authorization: `Bearer ${localStorage.getItem(AUTH_TOKEN)}`,
     },
   });
 
-export const API_USERS = () =>
-  axios.create({
-    baseURL: `${process.env.REACT_APP_USERS_API}`,
-    headers: {
-      authorization: `Bearer ${localStorage.getItem(AUTH_TOKEN)}`,
-    },
-  });
+export const API_QUESTIONS = () => API_GATEWAY("questions");
 
-export const API_TESTS = () =>
-  axios.create({
-    baseURL: `${process.env.REACT_APP_TESTS_API}`,
-    headers: {
-      authorization: `Bearer ${localStorage.getItem(AUTH_TOKEN)}`,
-    },
-  });
+export const API_USERS = () => API_GATEWAY("users");
+
+export const API_TESTS = () => API_GATEWAY("tests");
